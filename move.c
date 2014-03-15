13935271caf84eb99c9b16cc7ba6130d302ba5d9
Guido Martínez <mtzguido@gmail.com>
2014-03-15 00:37:10 -0300

    Se pasaron los move al .h (inline)

--- move.c
+++ move.c
@@ -8,50 +8,13 @@ static int sign(int a) {
 	return 0;
 }
 
-static int pawnMove(game g, int r, int c, int R, int C);
-static int rookMove(game g, int r, int c, int R, int C);
-static int kingMove(game g, int r, int c, int R, int C);
-static int knightMove(game g, int r, int c, int R, int C);
-static int bishopMove(game g, int r, int c, int R, int C);
+int pawnMove(game g, int r, int c, int R, int C);
+int rookMove(game g, int r, int c, int R, int C);
+int kingMove(game g, int r, int c, int R, int C);
+int knightMove(game g, int r, int c, int R, int C);
+int bishopMove(game g, int r, int c, int R, int C);
 
-int canMove(game g, int r, int c, int R, int C) {
-	if (r < 0 || r >= 8) return 0;
-	if (R < 0 || R >= 8) return 0;
-	if (c < 0 || c >= 8) return 0;
-	if (C < 0 || C >= 8) return 0;
-	if (r == R && c == C) return 0;
-
-	switch(g->board[r][c]) {
-	case 0:
-		return 0;
-	case WPAWN:
-	case BPAWN:
-		return pawnMove(g, r, c, R, C);
-	case WKNIGHT:
-	case BKNIGHT:
-		return knightMove(g, r, c, R, C);
-	case WBISHOP:
-	case BBISHOP:
-		return bishopMove(g, r, c, R, C);
-	case WROOK:
-	case BROOK:
-		return rookMove(g, r, c, R, C);
-	case WQUEEN:
-	case BQUEEN:
-	  return rookMove(g, r, c, R, C)
-	  	|| bishopMove(g, r, c, R, C);
-	case WKING:
-	case BKING:
-		return kingMove(g, r, c, R, C);
-	default:
-	  fprintf(stderr, "!!!!!!!! (%i)\n", g->board[r][c]);
-	  assert(0);
-	}
-
-	return -1;
-}
-
-static int pawnMove(game g, int r, int c, int R, int C) {
+int pawnMove(game g, int r, int c, int R, int C) {
 	if (colorOf(g->board[r][c]) == WHITE) {
 		if (c == C) {
 			if (R == r-1 && g->board[R][C] == 0)
@@ -89,7 +52,7 @@ static int pawnMove(game g, int r, int c, int R, int C) {
 	return 0;
 }
 
-static int knightMove(game g, int r, int c, int R, int C) {
+int knightMove(game g, int r, int c, int R, int C) {
 	if (abs(r-R) + abs(c-C) != 3 || abs(r-R) == 0 || abs(c-C) == 0)
 		return 0;
 
@@ -99,7 +62,7 @@ static int knightMove(game g, int r, int c, int R, int C) {
 		return 0;
 }
 
-static int bishopMove(game g, int r, int c, int R, int C) {
+int bishopMove(game g, int r, int c, int R, int C) {
 	int dr, dc;
 	int i, j;
 	if (abs(r-R) != abs(c-C))
@@ -118,7 +81,7 @@ static int bishopMove(game g, int r, int c, int R, int C) {
 	return 1;
 }
 
-static int rookMove(game g, int r, int c, int R, int C) {
+int rookMove(game g, int r, int c, int R, int C) {
 	if (r == R) {
 		int i;
 		int dc = sign(C-c);
@@ -145,7 +108,7 @@ static int rookMove(game g, int r, int c, int R, int C) {
 		return 0;
 }
 
-static int kingMove(game g, int r, int c, int R, int C) {
+int kingMove(game g, int r, int c, int R, int C) {
 	if (abs(C-c) > 1) return 0;
 	if (abs(R-r) > 1) return 0;
 	if (g->board[R][C] != 0 && colorOf(g->board[R][C]) == colorOf(g->board[r][c]))
--- move.h
+++ move.h
@@ -2,8 +2,47 @@
 #define __MOVE_H__
 
 #include "board.h"
+#include <assert.h>
+#include <stdio.h>
 
-int canMove(game g, int r, int c, int R, int C);
+int pawnMove(game g, int r, int c, int R, int C);
+int rookMove(game g, int r, int c, int R, int C);
+int kingMove(game g, int r, int c, int R, int C);
+int knightMove(game g, int r, int c, int R, int C);
+int bishopMove(game g, int r, int c, int R, int C);
+
+static inline int canMove(game g, int r, int c, int R, int C) {
+	const int piece = g->board[r][c];
+	switch(piece) {
+	case 0:
+		return 0;
+	case WPAWN:
+	case BPAWN:
+		return pawnMove(g, r, c, R, C);
+	case WKNIGHT:
+	case BKNIGHT:
+		return knightMove(g, r, c, R, C);
+	case WBISHOP:
+	case BBISHOP:
+		return bishopMove(g, r, c, R, C);
+	case WROOK:
+	case BROOK:
+		return rookMove(g, r, c, R, C);
+	case WQUEEN:
+	case BQUEEN:
+	  return rookMove(g, r, c, R, C)
+	  	|| bishopMove(g, r, c, R, C);
+	case WKING:
+	case BKING:
+		return kingMove(g, r, c, R, C);
+	default:
+	  fprintf(stderr, "!!!!!!!! (%i)\n", g->board[r][c]);
+	  assert(0);
+	}
+
+	return -1;
+}
+
 
 #endif
 
