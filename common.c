9299887eb9338d27921d7b99c2666e081d5dd860
Guido Martínez <guido@vanguardiasur.com.ar>
2014-06-30 16:10:00 -0300

    common: fix indenting

--- common.c
+++ common.c
@@ -15,19 +15,19 @@ int isPrefix(char *a, char *b) {
 
 char pieceOf(char c) {
 	switch (c) {
-		case 'P':   return WPAWN;
-		case 'p':   return WPAWN;
-		case 'R':   return WROOK;
-		case 'r':   return WROOK;
-		case 'N':   return WKNIGHT;
-		case 'n':   return WKNIGHT;
-		case 'B':   return WBISHOP;
-		case 'b':   return WBISHOP;
-		case 'Q':   return WQUEEN;
-		case 'q':   return WQUEEN;
-		case 'k':   return WKING;
-		case 'K':   return WKING;
-		default:    return EMPTY;
+		case 'P':	return WPAWN;
+		case 'p':	return WPAWN;
+		case 'R':	return WROOK;
+		case 'r':	return WROOK;
+		case 'N':	return WKNIGHT;
+		case 'n':	return WKNIGHT;
+		case 'B':	return WBISHOP;
+		case 'b':	return WBISHOP;
+		case 'Q':	return WQUEEN;
+		case 'q':	return WQUEEN;
+		case 'k':	return WKING;
+		case 'K':	return WKING;
+		default:	return EMPTY;
 	}   
 }
 
