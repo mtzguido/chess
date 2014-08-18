e20dd001fe72debc23a654b46298d352714ad2b9
Guido Martínez <guido@vanguardiasur.com.ar>
2014-08-18 15:07:26 -0300

    make_tests: use --version instead of git-describe

--- scripts/make_tests.sh
+++ scripts/make_tests.sh
@@ -74,7 +74,7 @@ msg "OPPONENT_PROG=$OPPONENT_PROG"
 msg "OPPONENT_ARGS=$OPPONENT_ARGS"
 msg "CHESS_PROG=$CHESS_PROG"
 msg "CHESS_ARGS=$CHESS_ARGS"
-msg "Git version: $(git describe --dirty --tags)"
+msg "Git version: $(./chess --version 2>&1 | head -n1)"
 msg
 msg "Running $total games"
 
