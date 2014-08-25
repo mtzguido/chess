2e42a8d941dd92d0389313ec0c033ebad4889f58
Guido Martínez <mtzguido@gmail.com>
2014-08-25 11:18:58 -0300

    addon: trivial: faster score
    And more asserts

--- addon_trivial.c
+++ addon_trivial.c
@@ -5,13 +5,10 @@
 
 /* Most valuable victim / least valuable attacker */
 static inline int mvv_lva(piece_t a, piece_t v) {
-	if (v != EMPTY)
-		return 10 * (v&7) - (a&7);
-	else
-		return 0;
+	return 10 * (v&7) - (a&7);
 }
 
-void trivial_score_succs(game g, int depth) {
+void trivial_score_succs(const game g, int depth) {
 	int i;
 
 	for (i=first_succ[ply]; i<first_succ[ply+1]; i++) {
@@ -20,12 +17,15 @@ void trivial_score_succs(game g, int depth) {
 		if (m.move_type != MOVE_REGULAR)
 			continue;
 
-		if (m.promote != 0)
+		if (m.promote != 0) {
+			assert(isPawn(g->board[m.r][m.c]));
+			assert(m.R == (g->turn == WHITE ? 0 : 7));
 			gsuccs[i].s += PROMOTE_SCORE;
+		}
 
-		if (enemy_piece(g, m.R, m.C))
+		if (enemy_piece(g, m.R, m.C)) {
 			gsuccs[i].s += CAPT_SCORE;
-
-		gsuccs[i].s += mvv_lva(g->board[m.r][m.c], g->board[m.R][m.C]);
+			gsuccs[i].s += mvv_lva(g->board[m.r][m.c], g->board[m.R][m.C]);
+		}
 	}
 }
