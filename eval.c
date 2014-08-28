6a2d037cfe001a79cf107dfec0d2193e97fa2f8c
Guido Martínez <guido@vanguardiasur.com.ar>
2014-08-27 23:18:11 -0300

    eval: split

--- eval.c
+++ eval.c
@@ -92,6 +92,13 @@ static inline score eval_bpawn(const int i, const int j) {
 	return ret;
 }
 
+static inline score eval_pawn(const u8 col, const int i, const int j) {
+	if (col == WHITE)
+		return eval_wpawn(i, j);
+	else
+		return eval_bpawn(i, j);
+}
+
 static const score shield_own[8] = {
 	[0] = -999999,	/* Impossible */
 	[1] = 0,	/* Never moved */
@@ -184,102 +191,77 @@ static inline score eval_bking(const int r, const int c) {
 	return ret;
 }
 
-static inline score eval_with_ranks(const game g) {
-	int i;
-	int bishop_count[2] = {0};
+static inline score eval_king(const u8 col, const int i, const int j) {
+	if (col == WHITE)
+		return eval_wking(i, j);
+	else
+		return eval_bking(i, j);
+}
+
+static inline score eval_with_ranks(const u8 col, const game g) {
+	int bishop_count = 0;
 	score score = 0;
 	u64 temp;
-	const u64 full = g->piecemask[WHITE] | g->piecemask[BLACK];
+	int i;
 
-	mask_for_each(full, temp, i) {
+	const int top = col == WHITE ? 0 : 7;
+	const int bot = col == WHITE ? 7 : 0;
+	const int opp = flipTurn(col);
+
+	mask_for_each(g->piecemask[col], temp, i) {
 		const int r = (i-1) / 8;
 		const int c = (i-1) % 8;
 		const piece_t piece = g->board[r][c];
 
-		switch (piece) {
+		switch (piece&7) {
 		/* Evaluate pawns individually */
 		case WPAWN:
-			score += eval_wpawn(r, c);
-			break;
-		case BPAWN:
-			score -= eval_bpawn(r, c);
+			score += eval_pawn(col, r, c);
 			break;
 
 		/* Penalize knight at end game */
 		case WKNIGHT:
 			score += interpolate(g, 0, KNIGHT_ENDGAME);
 			break;
-		case BKNIGHT:
-			score -= interpolate(g, 0, KNIGHT_ENDGAME);
-			break;
 
 		/* Count bishops */
 		case WBISHOP:
-			bishop_count[WHITE]++;
-			break;
-		case BBISHOP:
-			bishop_count[BLACK]++;
+			bishop_count++;
 			break;
 
 		/* Bonus for rook on (semi)open files */
 		case WROOK:
-			if (pawn_rank[WHITE][c+1] == 0) {
-				if (pawn_rank[BLACK][c+1] == 7)
+			if (pawn_rank[col][c+1] == top) {
+				if (pawn_rank[opp][c+1] == bot)
 					score += ROOK_OPEN_FILE;
 				else
 					score += ROOK_SEMI_OPEN_FILE;
 			}
 			break;
-		case BROOK:
-			if (pawn_rank[BLACK][c+1] == 7) {
-				if (pawn_rank[WHITE][c+1] == 0)
-					score -= ROOK_OPEN_FILE;
-				else
-					score -= ROOK_SEMI_OPEN_FILE;
-			}
-			break;
 
 		/* Evaluate king safety */
 		case WKING:
-			score += eval_wking(r, c)
-				* g->pieceScore[BLACK] / SIDE_SCORE ;
-			break;
-
-		case BKING:
-			score -= eval_bking(r, c)
-				* g->pieceScore[WHITE] / SIDE_SCORE ;
+			score += eval_king(col, r, c)
+				* g->pieceScore[opp] / SIDE_SCORE ;
 			break;
 		}
 	}
 
-	if (bishop_count[WHITE] > 1) score += DOUBLE_BISHOP;
-	if (bishop_count[BLACK] > 1) score -= DOUBLE_BISHOP;
+	if (bishop_count > 1) score += DOUBLE_BISHOP;
 
 	return score;
 }
 
-static inline score castle_score(const game g) {
-	score score = 0;
-
-	if (!g->castled[WHITE]) {
-		switch(2*g->castle_king[WHITE] + g->castle_queen[WHITE]) {
-		case 0x00: score += CASTLE_NN; break;
-		case 0x01: score += CASTLE_NY; break;
-		case 0x02: score += CASTLE_YN; break;
-		case 0x03: score += CASTLE_YY; break;
+static inline score castle_score(const u8 col, const game g) {
+	if (!g->castled[col]) {
+		switch(2*g->castle_king[col] + g->castle_queen[col]) {
+		case 0x00: return CASTLE_NN;
+		case 0x01: return CASTLE_NY;
+		case 0x02: return CASTLE_YN;
+		case 0x03: return CASTLE_YY;
 		}
 	}
-
-	if (!g->castled[BLACK]) {
-		switch(2*g->castle_king[BLACK] + g->castle_queen[BLACK]) {
-		case 0x00: score -= CASTLE_NN; break;
-		case 0x01: score -= CASTLE_NY; break;
-		case 0x02: score -= CASTLE_YN; break;
-		case 0x03: score -= CASTLE_YY; break;
-		}
-	}
-
-	return score;
+	return 0;
 }
 
 score boardEval(const game g) {
@@ -306,10 +288,12 @@ score boardEval(const game g) {
 	 * Segunda pasada. Con la información de los peones
 	 * evaluamos filas abiertas y status de peones
 	 */
-	score += eval_with_ranks(g);
+	score += eval_with_ranks(WHITE, g);
+	score -= eval_with_ranks(BLACK, g);
 
 	/* Penalizamos según posibilidades de enroque */
-	score += castle_score(g);
+	score += castle_score(WHITE, g);
+	score -= castle_score(BLACK, g);
 
 	if (inCheck(g, WHITE)) score += INCHECK;
 	if (inCheck(g, BLACK)) score -= INCHECK;
