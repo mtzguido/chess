0bf3a117ba34db7fe74d4635899a9dfe2ec5a509
Guido Martínez <mtzguido@gmail.com>
2014-09-05 19:20:40 -0300

    inplace: port move.c and move.h

--- move.c
+++ move.c
@@ -8,41 +8,41 @@ static int sign(int a) {
 	return 0;
 }
 
-bool pawnMove(game g, int r, int c, int R, int C);
-bool rookMove(game g, int r, int c, int R, int C);
-bool kingMove(game g, int r, int c, int R, int C);
-bool knightMove(game g, int r, int c, int R, int C);
-bool bishopMove(game g, int r, int c, int R, int C);
+bool pawnMove(int r, int c, int R, int C);
+bool rookMove(int r, int c, int R, int C);
+bool kingMove(int r, int c, int R, int C);
+bool knightMove(int r, int c, int R, int C);
+bool bishopMove(int r, int c, int R, int C);
 
-bool pawnMove(game g, int r, int c, int R, int C) {
-	if (colorOf(g->board[r][c]) == WHITE) {
+bool pawnMove(int r, int c, int R, int C) {
+	if (colorOf(G->board[r][c]) == WHITE) {
 		if (c == C) {
-			if (R == r-1 && g->board[R][C] == 0)
+			if (R == r-1 && G->board[R][C] == 0)
 				return 1;
-			else if (R == r-2 && r == 6 && g->board[r-1][c] == 0 && g->board[r-2][c] == 0)
+			else if (R == r-2 && r == 6 && G->board[r-1][c] == 0 && G->board[r-2][c] == 0)
 				return 1;
 			else
 				return 0;
 		} else if ((R == r-1) && (c == C+1 || c == C-1)) {
-			if (g->board[R][C] != 0 && colorOf(g->board[R][C]) != colorOf(g->board[r][c]))
+			if (G->board[R][C] != 0 && colorOf(G->board[R][C]) != colorOf(G->board[r][c]))
 				return 1;
-			else if (R == g->en_passant_x && C == g->en_passant_y)
+			else if (R == G->en_passant_x && C == G->en_passant_y)
 				return 1;
 			else
 				return 0;
 		}
-	} else { /* colorOf(g->board[r][c]) == BLACK */
+	} else { /* colorOf(G->board[r][c]) == BLACK */
 		if (c == C) {
-			if (R == r+1 && g->board[R][C] == 0)
+			if (R == r+1 && G->board[R][C] == 0)
 				return 1;
-			else if (R == r+2 && r == 1 && g->board[r+1][c] == 0 && g->board[r+2][c] == 0)
+			else if (R == r+2 && r == 1 && G->board[r+1][c] == 0 && G->board[r+2][c] == 0)
 				return 1;
 			else
 				return 0;
 		} else if ((R == r+1) && (c == C+1 || c == C-1)) {
-			if (g->board[R][C] != 0 && colorOf(g->board[R][C]) != colorOf(g->board[r][c]))
+			if (G->board[R][C] != 0 && colorOf(G->board[R][C]) != colorOf(G->board[r][c]))
 				return 1;
-			else if (R == g->en_passant_x && C == g->en_passant_y)
+			else if (R == G->en_passant_x && C == G->en_passant_y)
 				return 1;
 			else
 				return 0;
@@ -52,17 +52,17 @@ bool pawnMove(game g, int r, int c, int R, int C) {
 	return 0;
 }
 
-bool knightMove(game g, int r, int c, int R, int C) {
+bool knightMove(int r, int c, int R, int C) {
 	if (abs(r-R) + abs(c-C) != 3 || abs(r-R) == 0 || abs(c-C) == 0)
 		return 0;
 
-	if (g->board[R][C] == 0 || colorOf(g->board[R][C]) != colorOf(g->board[r][c]))
+	if (G->board[R][C] == 0 || colorOf(G->board[R][C]) != colorOf(G->board[r][c]))
 		return 1;
 	else
 		return 0;
 }
 
-bool bishopMove(game g, int r, int c, int R, int C) {
+bool bishopMove(int r, int c, int R, int C) {
 	int dr, dc;
 	int i, j;
 	if (abs(r-R) != abs(c-C))
@@ -72,24 +72,24 @@ bool bishopMove(game g, int r, int c, int R, int C) {
 	dc = sign(C-c);
 
 	for (j=c+dc, i=r+dr; j != C && i != R; i+=dr, j+=dc)
-		if (g->board[i][j] != 0)
+		if (G->board[i][j] != 0)
 			return 0;
 
-	if (g->board[R][C] != 0 && colorOf(g->board[R][C]) == colorOf(g->board[r][c]))
+	if (G->board[R][C] != 0 && colorOf(G->board[R][C]) == colorOf(G->board[r][c]))
 		return 0;
 
 	return 1;
 }
 
-bool rookMove(game g, int r, int c, int R, int C) {
+bool rookMove(int r, int c, int R, int C) {
 	if (r == R) {
 		int i;
 		int dc = sign(C-c);
 		for (i=c+dc; i != C; i += dc)
-			if (g->board[r][i] != 0)
+			if (G->board[r][i] != 0)
 				return 0;
 
-		if (g->board[R][C] != 0 && colorOf(g->board[R][C]) == colorOf(g->board[r][c]))
+		if (G->board[R][C] != 0 && colorOf(G->board[R][C]) == colorOf(G->board[r][c]))
 			return 0;
 
 		return 1;
@@ -97,10 +97,10 @@ bool rookMove(game g, int r, int c, int R, int C) {
 		int i;
 		int dr = sign(R-r);
 		for (i=r+dr; i != R; i += dr)
-			if (g->board[i][C] != 0)
+			if (G->board[i][C] != 0)
 				return 0;
 
-		if (g->board[R][C] != 0 && colorOf(g->board[R][C]) == colorOf(g->board[r][c]))
+		if (G->board[R][C] != 0 && colorOf(G->board[R][C]) == colorOf(G->board[r][c]))
 			return 0;
 
 		return 1;
@@ -108,10 +108,10 @@ bool rookMove(game g, int r, int c, int R, int C) {
 		return 0;
 }
 
-bool kingMove(game g, int r, int c, int R, int C) {
+bool kingMove(int r, int c, int R, int C) {
 	if (abs(C-c) > 1) return 0;
 	if (abs(R-r) > 1) return 0;
-	if (g->board[R][C] != 0 && colorOf(g->board[R][C]) == colorOf(g->board[r][c]))
+	if (G->board[R][C] != 0 && colorOf(G->board[R][C]) == colorOf(G->board[r][c]))
 		return 0;
 
 	return 1;
--- move.h
+++ move.h
@@ -7,38 +7,39 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool pawnMove(game g, int r, int c, int R, int C);
-bool rookMove(game g, int r, int c, int R, int C);
-bool kingMove(game g, int r, int c, int R, int C);
-bool knightMove(game g, int r, int c, int R, int C);
-bool bishopMove(game g, int r, int c, int R, int C);
+bool pawnMove(int r, int c, int R, int C);
+bool rookMove(int r, int c, int R, int C);
+bool kingMove(int r, int c, int R, int C);
+bool knightMove(int r, int c, int R, int C);
+bool bishopMove(int r, int c, int R, int C);
 
 static inline bool canMove(game g, int r, int c, int R, int C) {
 	const int piece = g->board[r][c];
+	G = g;
 	switch(piece) {
 	case EMPTY:
 		return false;
 	case WPAWN:
 	case BPAWN:
-		return pawnMove(g, r, c, R, C);
+		return pawnMove(r, c, R, C);
 	case WKNIGHT:
 	case BKNIGHT:
-		return knightMove(g, r, c, R, C);
+		return knightMove(r, c, R, C);
 	case WBISHOP:
 	case BBISHOP:
-		return bishopMove(g, r, c, R, C);
+		return bishopMove(r, c, R, C);
 	case WROOK:
 	case BROOK:
-		return rookMove(g, r, c, R, C);
+		return rookMove(r, c, R, C);
 	case WQUEEN:
 	case BQUEEN:
-		return rookMove(g, r, c, R, C)
-		    || bishopMove(g, r, c, R, C);
+		return rookMove(r, c, R, C)
+		    || bishopMove(r, c, R, C);
 	case WKING:
 	case BKING:
-		return kingMove(g, r, c, R, C);
+		return kingMove(r, c, R, C);
 	default:
-		fprintf(stderr, "!!!!!!!! (%i)\n", g->board[r][c]);
+		fprintf(stderr, "!!!!!!!! (%i)\n", G->board[r][c]);
 		abort();
 	}
 }
