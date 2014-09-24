9d66658a7f2dadaeec5d72865ce50bf82abd5cfa
Guido Martínez <mtzguido@gmail.com>
2014-09-24 19:11:05 -0300

    addon: trivial: tidy up

--- addon_trivial.c
+++ addon_trivial.c
@@ -12,20 +12,22 @@ void trivial_score_succs(int depth) {
 	int i;
 
 	for (i = first_succ[ply]; i < first_succ[ply+1]; i++) {
-		move m = gsuccs[i].m;
+		const move m = gsuccs[i].m;
+		const piece_t our   = G->board[m.r][m.c];
+		const piece_t enemy = G->board[m.R][m.C];
 
 		if (m.move_type != MOVE_REGULAR)
 			continue;
 
 		if (m.promote != 0) {
-			assert(isPawn(G->board[m.r][m.c]));
+			assert(isPawn(our));
 			assert(m.R == (G->turn == WHITE ? 0 : 7));
 			gsuccs[i].s += PROMOTE_SCORE;
 		}
 
 		if (enemy_piece(m.R, m.C)) {
 			gsuccs[i].s += CAPT_SCORE;
-			gsuccs[i].s += mvv_lva(G->board[m.r][m.c], G->board[m.R][m.C]);
+			gsuccs[i].s += mvv_lva(our, enemy);
 		}
 	}
 }
